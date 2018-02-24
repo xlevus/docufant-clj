9b5517f1a2eda085f728087daebddd23a94856f5
Chris Targett <chris@xlevus.net>
2018-02-24 23:01:10 +0000

    lein new docufant

---
+++ src/docufant/core.clj
@@ -0,0 +1,6 @@
+(ns docufant.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/docufant/core_test.clj
@@ -0,0 +1,7 @@
+(ns docufant.core-test
+  (:require [clojure.test :refer :all]
+            [docufant.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
