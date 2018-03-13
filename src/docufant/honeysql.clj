f66359d1f096d718798f1ff2dcce72ba914296e6
Chris Targett <chris@xlevus.net>
2018-03-13 16:02:54 +0000

    added honeysql jsonb funcs

---
+++ src/docufant/honeysql.clj
@@ -0,0 +1,49 @@
+(ns docufant.honeysql
+  (:require [honeysql.format :as fmt]
+            [docufant.postgres :as pg]))
+
+
+(defmulti cast-handler
+  "Format a sql string `q` to a cast of type `t`."
+  (fn [t q] t))
+
+(defmethod format-cast :default [t _] (throw (Exception. "Unknown type" t)))
+(defmethod cast-handler nil [_ q] (str "(" q ")"))
+(defmethod cast-handler Long [_ q] (str "(" q ")::int"))
+(defmethod cast-handler Double [_ q] (str "(" q ")::float"))
+
+
+(defn json-path
+  ([field] (fmt/to-sql field))
+  ([field path] (json-path field path nil))
+  ([field path type]
+   (println type)
+   (cast-handler type
+                 (str (fmt/to-sql field)
+                      " "
+                      (if (coll? path) "#" "-")
+                      (if type ">>" ">")
+                      " "
+                      (fmt/to-sql (pg/json-path path)))
+   )))
+
+
+(defn build-fn-handler [operator]
+  (defmethod fmt/fn-handler (str "jsonb_" operator)
+    ([op field value] (fmt/fn-handler op field (pg/jsonb value) nil))
+    ([_ [field & path] value type]
+     (str (json-path field path type) " " operator " " (fmt/to-sql value)))))
+
+
+;; (defmethod fmt/fn-handler "jsonb_="
+;;   ([op field value] (fmt/fn-handler op field (pg/jsonb value) nil))
+;;   ([_ [field & path] value type]
+;;    (str (json-path field path type) " = " (fmt/to-sql value))))
+
+
+(build-fn-handler "=")
+(build-fn-handler ">")
+(build-fn-handler ">=")
+(build-fn-handler "<")
+(build-fn-handler "<=")
+(build-fn-handler "<>")
